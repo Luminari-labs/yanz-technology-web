c1d8f545501cf6cd91c732c5f0f63163380555df
root <root@luminarilabs.cloud>
2026-04-27 21:13:50 +0000

    static-fixes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'tienda/:category',
+    renderMode: RenderMode.Server
+  },
+  {
     path: 'producto/:id',
     renderMode: RenderMode.Server
   },
